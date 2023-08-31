1eb87718b1d243e769bfe7eedde6d5259127acab
Valeriy <masterbox06@gmail.com>
2023-08-31 21:32:30 +0400

    lesson - 7

--- main.js
+++ main.js
@@ -9,35 +9,32 @@ const repeatString = (str, times, separator) => {
 // console.log(repeatString("yo", 1, ", "))
 
 // 2
-const checkStart = (str, subStr) => {
-  return str.toLowerCase().startsWith(subStr.toLowerCase())
-}
+const checkStart = (str, subStr) => str.toLowerCase().startsWith(subStr.toLowerCase())
 // console.log(checkStart("Incubator", "inc"))
 
 // 3
-const truncateString = (str, maxLength) => {
-  return str.split("").slice(0, maxLength).join("") + "..."
-}
+const truncateString = (str, maxLength) => str.split("").slice(0, maxLength).join("") + "..."
 // console.log(truncateString("lorem ipsum dolor sit amet", 8))
 
 // 4
-const getMinLengthWord = (str) => {
-  if (typeof str !== "string") return null
-  return str.split(" ").sort((a, b) => a.length - b.length)[0]
-}
-// console.log(getMinLengthWord("bifwgw iwgniwngw jwjfjwf qj"))
+const getMinLengthWord = (str) => typeof str !== "string" ? null : str.split(" ").sort((a, b) => a.length - b.length)[0]
+console.log(getMinLengthWord(null))
 
 // 5
-const setUpperCase = (str) => {
-  return str.toLowerCase().split(" ").map(w => w.charAt(0).toUpperCase() + w.slice(1)).join(" ")
-}
+const setUpperCase = (str) => str.toLowerCase().split(" ").map(w => w.charAt(0).toUpperCase() + w.slice(1)).join(" ")
 // console.log(setUpperCase("incuBator Wobf wnHWJ"))
 
 // 6
 const hasLetters = (str, letters) => {
-  for (const l of letters.split("")) {
-    if (!str.split("").includes(l)) return false
+  const lettersArray = [...letters.toLowerCase()]
+
+  for (const letter of lettersArray) {
+    const strParts = str.toLowerCase().split(letter)
+
+    if (strParts.length !== lettersArray.join('').split(letter).length) {
+      return false
+    }
   }
   return true
 }
-console.log(hasLetters("incuBator Wobf wnHWJ", "bw"))
+// console.log(hasLetters("ada", "aa"))
