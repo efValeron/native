7651c72d7ef5b33c7442dec8574e38e7cc938a96
Valeriy <masterbox06@gmail.com>
2023-10-18 20:46:27 +0300

    lesson - 14 classes

--- main.js
+++ main.js
@@ -1,116 +1,115 @@
-// this
-
-// 1. Global scope
-// 2. Arrow function scope -> function -> methods
-// 3. Call, apply, bind
-// 4. function constructor
-
-
-// 1. Global scope
-
-// console.log(this)
-
-
-// 2. Arrow function scope -> function -> methods
-
-// const a = () => {
-//   console.log(this)
-// }
-// a()
-
-// const car = {
-//   brand: "bmw",
-//   showBrand: () => {
-//     console.log(this.brand)
-//   }
-// }
-//
-// car.showBrand()
-
-// "use strict"
-// function foo() {
-//   console.log(this)
-// }
-//
-// foo()
-
-
-// const foo = () => {
-//   console.log(this.brand)
-// }
-
-// let car = {
-//   brand: "bmw",
-//   showBrand() {
-//     console.log(this.brand)
-//   }
-// }
-//
-// // car.showBrand()
-// const newCar = car
-// car = null
-// newCar.showBrand()
+// class
 
 // const car1 = {
-//   brand: "bmw"
-// }
-//
-// const car2 = {
-//   brand: "mercedes"
-// }
-//
-// function showBrand() {
-//   console.log(this.brand)
-// }
-//
-// car1.sb = showBrand
-// car2.sb = showBrand
-//
-// car1.sb()
-// car2.sb()
-
-
-// 3. Call, apply, bind
-
-// const scooter = {
-//   color: "yamaha",
-//   showBrand() {
-//     console.log(this.color)
-//   }
-// }
-//
-// const car1 = {
-//   color: "red",
-//   showColor() {
-//     console.log(this.color)
+//   brand: "bmw",
+//   maxSpeed: 200,
+//   startEngine() {
+//     console.log(`Start ${this.brand}`)
 //   }
 // }
 //
 // const car2 = {
-//   color: "yellow",
-//   showColor() {
-//     console.log(this.color)
+//   brand: "kia",
+//   maxSpeed: 220,
+//   startEngine() {
+//     console.log(`Start ${this.brand}`)
 //   }
 // }
 
-// car2.showColor.call(car1, 10, 20)
-// car2.showColor.apply(car1, [10, 20])
-// car2.showColor.bind(car1, 10, 20)()
 
-// car2.showColor.bind(scooter).call(car1)
-
-
-// 4. function constructor
-
-// function Car(brand, color) {
-//   // {}
-//   this.brand = brand // {brand: "bmw"}
-//   this.color = color // {brand: "bmw", color: "red"}
-//   // return {brand: "bmw", color: "red"}
+// function createCar(brand, maxSpeed) {
+//   return {
+//     brand,
+//     maxSpeed,
+//     startEngine() {
+//       console.log(`Start ${this.brand}`)
+//     }
+//   }
 // }
 //
-// const bmw = new Car("bmw", "red")
-// const audi = new Car("audi", "black")
+// const car1 = createCar("bmw", 200)
+// const car2 = createCar("kia", 220)
+// console.log(car1)
+// console.log(car2)
+
+
+// function Car(brand, maxSpeed) {
+//   this.brand = brand
+//   this.maxSpeed = maxSpeed
+//   // this.startEngine = function () {
+//   //   console.log(`Start ${this.brand}`)
+//   // }
+// }
 //
-// console.log(bmw)
-// console.log(audi)
+// Car.prototype.startEngine = function () {
+//   console.log(`Start ${this.brand}`)
+// }
+// Car.prototype.showMaxSpeed = function () {
+//   console.log(`Max speed: ${this.maxSpeed}`)
+// }
+//
+// const car1 = new Car("bmw", 200)
+// const car2 = new Car("kia", 220)
+// car1.startEngine()
+// car2.showMaxSpeed()
+
+
+class Car {
+  #brand
+
+  constructor(brand, maxSpeed) {
+    this.#brand = brand
+    this.maxSpeed = maxSpeed
+  }
+
+  startEngine() {
+    console.log(`Start ${this.#brand}`)
+  }
+
+  showMaxSpeed() {
+    console.log(`Max speed: ${this.maxSpeed}`)
+  }
+
+  get brand() {
+    return this.#brand
+  }
+
+  set brand(newBrand) {
+    this.#brand = newBrand
+  }
+
+  static whoFaster(car1, car2) {
+    return car1.maxSpeed > car2.maxSpeed ? car1.brand : car2.brand
+  }
+}
+
+const car1 = new Car("bmw", 200)
+const car2 = new Car("kia", 220)
+
+// console.log(car1)
+// console.log(car2)
+
+// console.log(car1.brand)
+// car1.brand = "toyota"
+// console.log(car1.brand)
+
+// console.log(`${Car.whoFaster(car1, car2)} faster`)
+
+
+class SuperCar extends Car {
+  constructor(brand, maxSpeed, wings) {
+    super(brand, maxSpeed)
+    this.wings = wings
+  }
+
+  fly() {
+    console.log(`I'm flying ${this.brand}`)
+  }
+}
+
+const superCar1 = new SuperCar("super porsche", 740, 4)
+
+console.log(superCar1)
+console.log(superCar1.brand)
+superCar1.fly()
+console.log(`${SuperCar.whoFaster(superCar1, car1)} faster`)
